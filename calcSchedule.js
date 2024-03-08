36cfc72bdf288d8d58498dedbd30c654e9e29bb2
Izayah Gibson <gibson.1258@osu.edu>
2024-03-08 13:08:21 -0500

    Added simple comments to calculateAmortization function

--- calcSchedule.js
+++ calcSchedule.js
@@ -1,15 +1,23 @@
 function calculateAmortizationSchedule(loanAmount, numMonths, interestRate) {
     const monthlyInterestRate = interestRate / 12 / 100;
     const monthlyPayment = loanAmount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -numMonths));
+
     let balance = loanAmount;
+    // Use array to hold the information for each month 
     let amortizationSchedule = [];
+
+    // loop throuch each month 
     for (let month = 1; month <= numMonths; month++) {
         const interestPaid = balance * monthlyInterestRate;
         const principalPaid = monthlyPayment - interestPaid;
+
+        // Upadte balance by subtracting the principal
+        // (Caused issues)
         balance -= principalPaid;
 
         amortizationSchedule.push({
             month: month,
+            // Ensure remaining payment cannot be less than 0
             principalRemaining: balance > 0 ? balance : 0,
             interestPaid: interestPaid,
             principalPaid: principalPaid,
