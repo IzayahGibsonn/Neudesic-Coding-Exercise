af8611bb173fc847cbd264ede8b48e9619886acb
Izayah Gibson <gibson.1258@osu.edu>
2024-03-08 13:03:19 -0500

    added for loop within calculateAmortization function to loop through each month

--- calcSchedule.js
+++ calcSchedule.js
@@ -3,6 +3,18 @@ function calculateAmortizationSchedule(loanAmount, numMonths, interestRate) {
     const monthlyPayment = loanAmount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -numMonths));
     let balance = loanAmount;
     let amortizationSchedule = [];
+    for (let month = 1; month <= numMonths; month++) {
+        const interestPaid = balance * monthlyInterestRate;
+        const principalPaid = monthlyPayment - interestPaid;
+        balance -= principalPaid;
 
+        amortizationSchedule.push({
+            month: month,
+            principalRemaining: balance > 0 ? balance : 0,
+            interestPaid: interestPaid,
+            principalPaid: principalPaid,
+            totalMonthlyPayment: monthlyPayment
+        });
+    }
     return amortizationSchedule;
 }
